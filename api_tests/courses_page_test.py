7dc10f488c9bd86f32ca968ec7bdd38efceba0f4
doisu <odmitry@akhter.studio>
2024-07-02 22:33:17 +0500

    course search test added

--- api_tests/courses_page_test.py
+++ api_tests/courses_page_test.py
@@ -1,7 +1,7 @@
 import requests
 # from ..api_tests.generator import random_name
 
-def test_courses_list(base_api_url, access_token, credentials):
+def test_courses_list(base_api_url, access_token):
     courses_number = 16
     path = f"/api/v1/courses/?limit={courses_number}&page=1"
 
@@ -13,4 +13,30 @@ def test_courses_list(base_api_url, access_token, credentials):
 
     if int(json_courses_list_data['count']) >= courses_number:
         assert len(json_courses_list_data['results']) == courses_number
+
+
+def test_courses_search(base_api_url, access_token):
+    path = "/api/v1/courses/"
+
+    json_courses_list_data = requests.get(url=base_api_url + path, 
+                                 headers={'Authorization': access_token['Authorization']}).json()
+    first_course_name = json_courses_list_data['results'][0]['name']
+    
+    query_params = {
+        'limit': 16,
+        'page': 1,
+        'search': first_course_name,
+        }
+    
+    course_search_response = requests.get(url=base_api_url + path, params=query_params,
+                                 headers={'Authorization': access_token['Authorization']})
+    
+    assert course_search_response.status_code == 200
+    json_course_search_data = course_search_response.json()
+    
+    if len(json_course_search_data['results']) > 0:
+        assert json_course_search_data['results'][0]['name'] == first_course_name
+    
+    
+
         
