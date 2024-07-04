94ac3b3c70b4b2c6bcdbbb3c3734092d6ae27a4b
doisu <odmitry@akhter.studio>
2024-07-04 19:34:07 +0500

    allure improvements added to courses page tests for reports; refactred names of asserts

--- api_tests/courses_page_test.py
+++ api_tests/courses_page_test.py
@@ -1,83 +1,118 @@
 import requests
+import allure
 # from ..api_tests.generator import random_name
 
+
+@allure.story('Courses list page')
+@allure.title('Retrieve the courses list')
 def test_courses_list(base_api_url, access_token):
     courses_number = 16
     path = f"/api/v1/courses/?limit={courses_number}&page=1"
 
-    courses_list_response = requests.get(url=base_api_url + path, 
-                                 headers={'Authorization': access_token.get('Authorization')})
+    with allure.step('Retrieve the courses list from the API'):
+        courses_list_response = requests.get(url=base_api_url + path, 
+                                    headers={'Authorization': access_token.get('Authorization')})
     
-    assert courses_list_response.status_code == 200
+    with allure.step('Check the 200 status response'):
+        assert courses_list_response.status_code == 200
     json_courses_list_data = courses_list_response.json()
 
+    with allure.step('Check whether the dictionary isnt blank in the response'):
+        assert json_courses_list_data != {}
+
     if int(json_courses_list_data.get('count')) >= courses_number:
-        assert len(json_courses_list_data.get('results')) == courses_number
+        with allure.step('Check whether courses number correspond with provided number'):
+            assert len(json_courses_list_data.get('results')) == courses_number
 
 
+@allure.story('Courses list page')
+@allure.title('Retrieve the courses categories list')
 def test_course_categories_list(base_api_url, access_token):
     path = "/api/v1/category/"
 
-    course_categories_list_response = requests.get(url=base_api_url + path, 
-                                 headers={'Authorization': access_token.get('Authorization')})
+    with allure.step('Retrieve the courses categories list from the API'):
+        course_categories_list_response = requests.get(url=base_api_url + path, 
+                                    headers={'Authorization': access_token.get('Authorization')})
     
-    assert course_categories_list_response.status_code == 200
+    with allure.step('Check the 200 status response'):  
+        assert course_categories_list_response.status_code == 200
     json_course_categories_data = course_categories_list_response.json()
 
     if len(json_course_categories_data) > 0:
-        assert json_course_categories_data != []
+        with allure.step('Check whether the list isnt blank in the response'):
+            assert json_course_categories_data != []
 
 
+@allure.story('Courses list page')
+@allure.title('Course search')
 def test_courses_search(base_api_url, access_token):
     path = "/api/v1/courses/"
 
-    json_courses_list_data = requests.get(url=base_api_url + path, 
-                                 headers={'Authorization': access_token.get('Authorization')}).json()
-    first_course_name = json_courses_list_data.get('results')[0].get('name')
+    with allure.step('Retrieve the courses list from the API'):
+        json_courses_list_data = requests.get(url=base_api_url + path, 
+                                    headers={'Authorization': access_token.get('Authorization')}).json()
     
-    query_params = {
-        'limit': 16,
-        'page': 1,
-        'search': first_course_name,
-        }
+    if int(json_courses_list_data.get('count')) > 0:
+        with allure.step('Retrieve the first course name'):
+            first_course_name = json_courses_list_data.get('results')[0].get('name')
     
-    course_search_response = requests.get(url=base_api_url + path, params=query_params,
-                                 headers={'Authorization': access_token.get('Authorization')})
+        with allure.step('Set query parameters with the course name'):
+            query_params = {
+                'limit': 16,
+                'page': 1,
+                'search': first_course_name,
+                }
+        
+        with allure.step('Get search results from the API'):
+            course_search_response = requests.get(url=base_api_url + path, params=query_params,
+                                        headers={'Authorization': access_token.get('Authorization')})
+        
+        with allure.step('Check the 200 status response'):  
+            assert course_search_response.status_code == 200
+        json_course_search_data = course_search_response.json()
+        
+        with allure.step('Verify provided course name with one in the response'):  
+            assert json_course_search_data.get('results')[0].get('name') == first_course_name
+    else:
+        print('Courses list is empty')
     
-    assert course_search_response.status_code == 200
-    json_course_search_data = course_search_response.json()
-    
-    if len(json_course_search_data.get('results')) > 0:
-        assert json_course_search_data.get('results')[0].get('name') == first_course_name
-    
-    
+
+@allure.story('Courses list page')
+@allure.title('Filter courses by category')
 def test_filter_courses_by_category(base_api_url, access_token):
     path = "/api/v1/courses/"
 
-    course_categories_list_response = requests.get(url=base_api_url + "/api/v1/category/", 
-                                 headers={'Authorization': access_token.get('Authorization')}).json()
+    with allure.step('Retrieve the courses categories list from the API'):
+        course_categories_list_response = requests.get(url=base_api_url + "/api/v1/category/", 
+                                    headers={'Authorization': access_token.get('Authorization')}).json()
     
-    query_params = {
-                    'limit': 16,
-                    'page': 1
-                    }
+    with allure.step('Set base query parameters'):
+        query_params = {
+                        'limit': 16,
+                        'page': 1
+                        }
+        
+    with allure.step('Set query parameters with category id'):
+        if len(course_categories_list_response) > 0:
+            for object in course_categories_list_response:
+                if 'Сметное дело' in object.values():
+                    category_id = object.get('id')
+                    query_params = {
+                        'limit': 16,
+                        'page': 1,
+                        'category': category_id
+                        }
     
-    if len(course_categories_list_response) > 0:
-        for object in course_categories_list_response:
-            if 'Сметное дело' in object.values():
-                category_id = object.get('id')
-                query_params = {
-                    'limit': 16,
-                    'page': 1,
-                    'category': category_id
-                    }
+    with allure.step('Retrieve the filtered courses list from the API'):
+        filter_courses_by_category_response = requests.get(url=base_api_url + path, params=query_params,
+                                    headers={'Authorization': access_token.get('Authorization')})
     
-    filter_courses_by_category_response = requests.get(url=base_api_url + path, params=query_params,
-                                 headers={'Authorization': access_token.get('Authorization')})
-    
-    assert filter_courses_by_category_response.status_code == 200
+    with allure.step('Check the 200 status response'): 
+        assert filter_courses_by_category_response.status_code == 200
     json_filter_courses_by_category_data = filter_courses_by_category_response.json()
-    assert json_filter_courses_by_category_data != {}
+    with allure.step('Check whether the dictionary isnt blank in the response'):
+        assert json_filter_courses_by_category_data != {}
     
     if len(json_filter_courses_by_category_data.get('results')) > 0 and query_params.get('category') is not None:
-        assert category_id in json_filter_courses_by_category_data.get('results')[0].get('category')
+        with allure.step('Verify provided category id with one in the response'):  
+            assert category_id in json_filter_courses_by_category_data.get('results')[0].get('category')
--- api_tests/profile_page_test.py
+++ api_tests/profile_page_test.py
@@ -3,27 +3,27 @@ from ..api_tests.generator import random_name
 import allure
 
 
-@allure.feature('Profile page')
-@allure.story('Retrieve users data')
+@allure.story('Profile page')
+@allure.title('Retrieve users data')
 def test_user_me(base_api_url, access_token, credentials):
     path = "/api/v1/auth/users/me/"
 
-    with allure.step('Retrieve users data from API'):
+    with allure.step('Retrieve users data from the API'):
         user_me_response = requests.get(url=base_api_url + path, 
                                     headers={'Authorization': access_token.get('Authorization')})
         json_user_me_data = user_me_response.json()
 
     with allure.step('Check the 200 status response'):
         assert user_me_response.status_code == 200
-    with allure.step('Check the blank object response'):
+    with allure.step('Check whether the dictionary isnt blank in the response'):
         assert json_user_me_data != {}
-    with allure.step('Verify username in the response'):
+    with allure.step('Verify the username with one in the response'):
         assert credentials.get('username') == json_user_me_data.get('username')
 
 
-@allure.feature('Profile page')
-@allure.story('Change users data')
-def test_change_user_data(base_api_url, access_token):
+@allure.story('Profile page')
+@allure.title('Change users data')
+def test_change_users_data(base_api_url, access_token):
     path = "/api/v1/profile/"
     
     with allure.step('Generate random users data'):
@@ -38,21 +38,21 @@ def test_change_user_data(base_api_url, access_token):
 
     with allure.step('Check the 200 status response'):
         assert changed_user_data_response.status_code == 200
-    with allure.step('Check the blank object response'):
+    with allure.step('Check whether the dictionary isnt blank in the response'):
         assert changed_user_data_response != {}
-    with allure.step('Verify users data in the response'):
-        with allure.step('Verify first_name in the response'):
+    with allure.step('Verify users data with one in the response'):
+        with allure.step('Verify first_name with one in the response'):
             assert json_random_data.get('first_name') == response_changed_data.get('first_name')
-        with allure.step('Verify last_name in the response'):
+        with allure.step('Verify last_name with one in the response'):
             assert json_random_data.get('last_name') == response_changed_data.get('last_name')
-        with allure.step('Verify patronymic in the response'):
+        with allure.step('Verify patronymic with one in the response'):
             assert json_random_data.get('patronymic') == response_changed_data.get('patronymic')
-        with allure.step('Verify phone in the response'):
+        with allure.step('Verify phone with one in the response'):
             assert json_random_data.get('phone') == response_changed_data.get('phone')
 
 
-@allure.feature('Profile page')
-@allure.story('Change users password')
+@allure.story('Profile page')
+@allure.title('Change users password')
 def test_change_users_password(base_api_url, access_token, credentials):
     path = "/api/v1/auth/users/set_password/"
 
