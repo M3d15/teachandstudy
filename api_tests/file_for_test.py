b3e0d6d8a05b6f4efe142289bd7a4d1f97ebb78d
doisu <odmitry@akhter.studio>
2024-07-16 19:54:05 +0500

    editing course test refactored; more checks added;

--- api_tests/file_for_test.py
+++ api_tests/file_for_test.py
@@ -5,14 +5,8 @@ fake_us = Faker('en_US')
 fake_ru = Faker('ru_RU')
 Faker.seed()
 
-def generate_course_name():
-    subjects = ['Mathematics', 'Physics', 'Chemistry', 'Biology', 'History', 'Geography', 'Literature', 'Art', 'Music', 'Computer Science']
-    course_types = ['Introduction to', 'Advanced', 'Basics of', 'Principles of', 'Fundamentals of', 'Theory of', 'Practical', 'Applications of']
-    level = random.choice(['101', '201', '301', '401'])
-    
-    course_name = f"{random.choice(course_types)} {random.choice(subjects)} {level}"
-    return course_name
+category_list = [1,2,3,4,5]
 
-# Generate and print 5 random course names
-for _ in range(5):
-    print(generate_course_name())
+random_category = random.choice(category_list)
+
+print(type(random_category))
--- api_tests/teaching_page_test.py
+++ api_tests/teaching_page_test.py
@@ -1,6 +1,7 @@
 import requests
 import allure
 import random
+import pytest
 from ..api_tests.generator import generate_course_name
 
 
@@ -15,13 +16,13 @@ def test_create_free_course(base_api_url, access_token):
                                     headers={'Authorization': access_token.get('Authorization')}).json()
 
     with allure.step('Retrieve the courses categories list from the API'):
-        course_categories_list_response = requests.get(url=base_api_url + '/api/v1/category/', 
+        json_course_categories_list_data = requests.get(url=base_api_url + '/api/v1/category/', 
                                     headers={'Authorization': access_token.get('Authorization')}).json()
     
     with allure.step('Check if categories number > 0'):
-        if len(course_categories_list_response) > 0:
+        if len(json_course_categories_list_data) > 0:
             with allure.step('Getting category ids from the response'):
-                for object in course_categories_list_response:
+                for object in json_course_categories_list_data:
                     category_list.append(object.get('id'))
                     if len(category_list) >= 10:
                         break
@@ -62,48 +63,80 @@ def test_create_free_course(base_api_url, access_token):
                 with allure.step('Verify course status'):
                     assert json_course_creating_data.get('status') == 'NEW'
                 with allure.step('Verify course category'):
-                    assert body.get('category')[0] in json_course_creating_data.get('category')
+                    assert json_course_creating_data.get('category') == body.get('category')
+                    # assert body.get('category')[0] in json_course_creating_data.get('category')
 
 
 @allure.story('Teaching page')
 @allure.title('Course editing')
 def test_edit_course(base_api_url, access_token):
-    with allure.step('Set query parameters'):
+    category_list = []
+
+    with allure.step('Set query parameters for user courses list'):
         query_params = {
-            'limit': 16,
+            'limit': 12,
             'page': 1,
             }
-    
     with allure.step('Retrieve course list'):
-        course_list_response = requests.get(url=base_api_url + '/api/v1/courses/teacher/', params=query_params,
+        json_course_list_data = requests.get(url=base_api_url + '/api/v1/courses/teacher/', params=query_params,
                                     headers={'Authorization': access_token.get('Authorization')}).json()
     
-    path = f"/api/v1/courses/{course_list_response.get('results')[-1].get('id')}/"
+    with allure.step('Check if courses number > 0'):
+        if len(json_course_list_data.get('results')) > 0:
+            with allure.step('Retrieve users data from the API'):
+                json_user_me_data = requests.get(url=base_api_url + "/api/v1/auth/users/me/", 
+                                            headers={'Authorization': access_token.get('Authorization')}).json()
 
-    with allure.step('Set query parameters'):
-        body = {
-            'name': 'course 3CHANGED',
-            'category': []
-            }
+            with allure.step('Retrieve the courses categories list from the API'):
+                json_course_categories_list_data = requests.get(url=base_api_url + '/api/v1/category/', 
+                                            headers={'Authorization': access_token.get('Authorization')}).json()
+            
+            with allure.step('Check if categories number > 0'):
+                if len(json_course_categories_list_data) > 0:
+                    with allure.step('Getting category ids from the response'):
+                        for object in json_course_categories_list_data:
+                            category_list.append(object.get('id'))
+                            if len(category_list) >= 10:
+                                break
+                    
+                    path = f"/api/v1/courses/{json_course_list_data.get('results')[-1].get('id')}/"
 
-    with allure.step('Course editing'):
-        course_editing_response = requests.put(url=base_api_url + path, json=body,
-                                    headers={'Authorization': access_token.get('Authorization')})
-    
-    with allure.step('Check the 200 status response'):
-        assert course_editing_response.status_code == 200
-    json_course_editing_data = course_editing_response.json()
+                    with allure.step('Set query parameters for course editing'):
+                        body = {
+                            'name': f'{generate_course_name()}',
+                            'category': [random.choice(category_list)],
+                            'certificate_scores': random.randint(1, 100),
+                            'academic_hours': random.randint(1, 200),
+                            'price': 0,
+                            'description': f'some course description 123 !@# {generate_course_name()}',
+                            'teachers': [json_user_me_data.get('id')]
+                            }
+                    
+                    with allure.step('Course editing'):
+                        course_editing_response = requests.put(url=base_api_url + path, json=body,
+                                                    headers={'Authorization': access_token.get('Authorization')})
+                    
+                    with allure.step('Check the 200 status response'):
+                        assert course_editing_response.status_code == 200
+                    json_course_editing_data = course_editing_response.json()
 
-    with allure.step('Check whether the list isnt blank in the response'):
-        assert json_course_editing_data != {}
-    
-    
-    with allure.step('Verify course data in the response'):
-        with allure.step('Verify course id'):
-            assert json_course_editing_data.get('id') == course_list_response.get('results')[-1].get('id')
-        with allure.step('Verify changed course name'):
-            assert json_course_editing_data.get('name') == body.get('name')
-        with allure.step('Verify changed course category'):
-            assert json_course_editing_data.get('category') == []
-
-
+                    with allure.step('Check whether the list isnt blank in the response'):
+                        assert json_course_editing_data != {}
+                    
+                    with allure.step('Verify course data in the response'):
+                        with allure.step('Verify course id'):
+                            assert json_course_editing_data.get('id') == json_course_list_data.get('results')[-1].get('id')
+                        with allure.step('Verify changed course name'):
+                            assert json_course_editing_data.get('name') == body.get('name')
+                        with allure.step('Verify changed course certificate scores'):
+                            assert json_course_editing_data.get('certificate_scores') == body.get('certificate_scores')
+                        with allure.step('Verify changed course academic hours'):
+                            assert json_course_editing_data.get('academic_hours') == body.get('academic_hours')
+                        with allure.step('Verify teacher id'):
+                            assert json_course_editing_data.get('teachers')[0] == body.get('teachers')[0]
+                        with allure.step('Verify course status'):
+                            assert json_course_editing_data.get('status') == 'NEW'
+                        with allure.step('Verify changed course category'):
+                            assert json_course_editing_data.get('category') == body.get('category')
+        else:
+            pytest.fail("There is no courses to edit")
